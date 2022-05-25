441c7053daee0b6ab040a1effd0cb168e3e60493
David Pires WorkPlace <developer.david@outlook.com>
2022-05-25 15:01:04 -0300

    feat: aula06

---
+++ aula06/index.js
@@ -0,0 +1,15 @@
+//variáveis escopo global e fora do escopo 
+//Não se pode começar o nome de uma variável com número
+//Não pode conter espaços ou traços
+//Utilizar camelCase para palavras compostas 
+//Case-Sensitive
+
+let nome = 'Matheus '
+
+var sobreNome = 'Gomes'
+
+nome = 'Jorge '
+
+sobreNome = 'Matheus'
+
+console.log(`Meu nome é: ` + nome + sobreNome);
