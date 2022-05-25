ab02dd9d6109f7e8e934bf9024fb3202b57fc44a
David Pires WorkPlace <developer.david@outlook.com>
2022-05-25 14:09:47 -0300

    feat: aula03, aula04

--- aula03/index.js
+++ aula03/index.js
@@ -1,1 +1,5 @@
 console.log('mostrar no terminal');
+console.log(`'mostrar' no "terminal"`); //template strings
+console.log(123456, 15.85, 'Este texto vai ser mostrado'); //template strings
+
+console.log(`Meu nome é "David Pires". Estou aprendendo JavaScript às ` + 10 + ` da manhã`);
