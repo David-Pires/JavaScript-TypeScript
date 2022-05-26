7f25780687198f984c60f5887a38946d31513a4f
David Pires WorkPlace <developer.david@outlook.com>
2022-05-26 17:12:39 -0300

    feat: aula11 - exercicio

--- aula11/js/index.js
+++ aula11/js/index.js
@@ -1,1 +1,12 @@
-//window.alert('')
+//window.alert('') 
+
+
+// let confirmation = confirm('Deseja confirmar')
+
+const msg1 = Number(prompt('Digite o 1º número'))
+const msg2 = Number(prompt('Digite o 2º número'))
+
+
+const soma = msg1 + msg2
+
+let resultado = alert(`A soma dos dois números é: ${msg1 + msg2}`)
