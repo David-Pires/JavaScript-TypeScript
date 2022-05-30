b5cb4a41b52397f8546a2b94dc055f37ea71df63
David Pires <developer.david@outlook.com>
2022-05-30 15:50:10 -0300

    feat: aula15

--- aula15/js/index.js
+++ aula15/js/index.js
@@ -2,10 +2,16 @@
 
 let num1 = 9.49
 
+let potencia = 16
+
+
+
 function pulaLinha() {
   document.body.innerHTML += "<br> "  
   return
 }
+
+const aleatorio = document.body.innerHTML += Math.round(Math.random() * (5 - 10) + 5) // Math.random() números aleatórios 
 
 document.body.innerHTML += Math.floor(num1)
 pulaLinha()
@@ -16,3 +22,10 @@ pulaLinha()
 document.body.innerHTML += Math.max(1000,21,350,578,8,9,8,548,2199,5161) // retorna o maior número
 pulaLinha()
 document.body.innerHTML += Math.min(1000,21,350,578,8,9,8,548,2199,5161)// retorna o menor número
+pulaLinha()
+document.body.innerHTML += Math.pow(4, 4) // potenciação 
+pulaLinha()
+document.body.innerHTML += 4**4; // potenciação 
+pulaLinha()
+document.body.innerHTML += 9**9; // potenciação 
+ 
