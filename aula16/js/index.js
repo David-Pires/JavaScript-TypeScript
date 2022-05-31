9646e8b53f6418c83dfbed492c3cb9daefca3d3c
David Pires WorkPlace <developer.david@outlook.com>
2022-05-31 13:56:30 -0300

    feat: aula16

---
+++ aula16/js/index.js
@@ -0,0 +1,20 @@
+// aula 16 - arrays 
+
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+
+document.body.innerHTML += `${alunos[2]= 'David'} `
+document.body.innerHTML += `<br />${alunos.length}`
+document.body.innerHTML += `<br />${alunos[3]= 'Flavio'} `
+document.body.innerHTML += `<br /> ${alunos.push('Carlos')}`
+document.body.innerHTML += `<br /> ${alunos}`
+document.body.innerHTML += `<br /> ${alunos.push('Gonzaga')}`
+document.body.innerHTML += `<br /> ${alunos}`
+document.body.innerHTML += `<br /> ${alunos.pop()}` //Nome Gonzaga sai do array no ultimo indice
+const removidoDoArray = document.body.innerHTML += `<br /> ${alunos.pop()}`
+document.body.innerHTML += `<br /> ${alunos}`
+document.body.innerHTML += alunos.shift() // remove o primeiro item o array
+document.body.innerHTML += `<br /> ${alunos}`
+
+
