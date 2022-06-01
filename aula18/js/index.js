67c19af0803ef6f94b6dbdd8293df28670d8f2a2
David Pires <developer.david@outlook.com>
2022-05-31 23:11:58 -0300

    feat: aula18 objetos

--- aula18/js/index.js
+++ aula18/js/index.js
@@ -1,50 +1,68 @@
-// aula 17  - Funções
+// aula 18  - Objetos
 
 
+// const array = [1,2,3]
+// array.push(4)
+// array[3] = 'David'
+// array.push(5)
+// document.body.innerHTML += array
 
-// function saudacao(nome) {
-  
-//   return `Bom dia ${nome}`
+// const nome01 = 'David'
+// const sobrenome01 = 'Pires'
+// const idade01 = 25
+
+// const nome02 = 'Maria'
+// const sobrenome2 = 'Souza'
+// const idade02 = 54
+
+// const pessoa = {
+//  nome: 'Barão',
+//  sobrenome: 'Maua',
+//  idade: 89
 // }
 
-// const variavel = saudacao('David')
-// console.log(variavel);
+// function criarPessoa(nome, sobrenome, idade) {
+//   return {
+//     nome,
+//     sobrenome,
+//     idade
+//   }
+   
+// }
+// const pessoa1 = criarPessoa('david', 'pires', 40);
+// const pessoa2 = criarPessoa('luiza', 'oliveira', 50);
+// const pessoa3 = criarPessoa('bruno', 'manha', 60);
+// const pessoa4 = criarPessoa('marco', 'bruno', 20);
+// const pessoa5 = criarPessoa('leandro', 'tarde', 10);
+// const pessoa6 = criarPessoa('cassio', 'arantes', 20);
+
+// console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade)
+// console.log(pessoa2.nome, pessoa2.sobrenome, pessoa2.idade)
+// console.log(pessoa3.nome, pessoa3.sobrenome, pessoa3.idade)
+// console.log(pessoa4.nome, pessoa4.sobrenome, pessoa4.idade)
+// console.log(pessoa5.nome, pessoa5.sobrenome, pessoa5.idade)
+// console.log(pessoa6.nome, pessoa6.sobrenome, pessoa6.idade)
+
+// document.body.innerHTML += `${pessoa.nome} <br />`
+// document.body.innerHTML += `${pessoa.sobrenome} <br />`
+// document.body.innerHTML += `${pessoa.idade} <br />`
 
 
-function soma(x, y) {
-  const resultado = x + y
-  return resultado
-  
+const pessoa = {
+  nome: 'Meu Nome',
+  sobrenome: 'Mira',
+  idade: 30,
+  //criar uma função aqui dentro
+  fala() {
+    console.log(`${this.idade} idade`);
+  },
+  incrementaIdade() {
+    this.idade++
+  }
 }
-console.log(soma(56,56));
 
-function subtracao(a,b) {
-  const resultadoSubtracao = a - b
-  return resultadoSubtracao
-}
-
-function soma2(x = 2, y = 4) {
-  const resultadoSoma = x + y
-  return resultadoSoma
-}
-
-//mais formas de criar funções
-
-const raiz = function(n) {
-  return n ** 0.5
-}
-
-const raiz2 = (n) => n ** 0.5 // se tiver só um parametro não precisa de (n)
-
-
-console.log(subtracao(45,36));
-
-
-
-console.log(soma);
-
-document.body.innerHTML += `${soma(4,5)} <br />`
-document.body.innerHTML += `<br /> ${subtracao(89, 67)}`
-document.body.innerHTML += `<br /> ${soma2()}`
-document.body.innerHTML += `<br /> ${raiz(9)}`
-document.body.innerHTML += `<br /> ${raiz2(16)}`
+pessoa.fala()
+pessoa.incrementaIdade()
+pessoa.fala()
+pessoa.incrementaIdade()
+pessoa.fala()
