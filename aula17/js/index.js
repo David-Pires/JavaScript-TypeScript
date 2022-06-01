f4e742446c99a4cfef88981b0704ab3015e40eed
David Pires <developer.david@outlook.com>
2022-05-31 21:52:50 -0300

    feat: aula17

---
+++ aula17/js/index.js
@@ -0,0 +1,30 @@
+// aula 17  - Funções
+
+
+
+// function saudacao(nome) {
+  
+//   return `Bom dia ${nome}`
+// }
+
+// const variavel = saudacao('David')
+// console.log(variavel);
+
+
+function soma(x, y) {
+  const resultado = x + y
+  return resultado
+  
+}
+console.log(soma(56,56));
+
+function subtracao(a,b) {
+  const resultadoSubtracao = a - b
+  return resultadoSubtracao
+}
+
+console.log(subtracao(45,36));
+
+
+
+console.log(soma);
