62b60bee11d22614550213a78adc7bb9591c4ef9
David Pires <developer.david@outlook.com>
2022-06-01 09:57:17 -0300

    feat: aula19

--- aula19/js/index.js
+++ aula19/js/index.js
@@ -2,3 +2,16 @@
 
 // Primitivos (imutáveis) - string, number, boolean, undefined,
 //  null (bigint, symbol)
+
+let a = 'A'
+let b = a
+
+a = 'Outra coisa'
+console.log(
+  a, b
+);
+
+//Referência (mutável) = array, object, function - Passados por referência 
+let c = [1, 2, 3]
+let d = c // let d aponta para o mesmo valor em memória 
+console.log(c, d);
