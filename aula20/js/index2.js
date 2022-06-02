a3fa5918689223aad6fcdee9271ef3f325051b8d
David Pires <developer.david@outlook.com>
2022-06-02 14:22:23 -0300

    feat: resolution

---
+++ aula20/js/index2.js
@@ -0,0 +1,34 @@
+
+function escopoTotal() {
+  const formulario = document.querySelector('.form')
+  const infoDoform = document.querySelector('.infoDoForm')
+
+  const array = []
+
+  function recebeEventoDoForm(evento) {
+    evento.preventDefault()
+
+    const nome = formulario.querySelector('.nome')
+    const sobrenome = formulario.querySelector('.sobrenome')
+    const peso = formulario.querySelector('.peso')
+    const altura = formulario.querySelector('.altura')
+    
+    
+    array.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+
+    })
+    console.log(array)
+    infoDoform.innerHTML += `${nome.value} ${sobrenome.value} <br />` +
+    `${peso.value} ${altura.value}`
+  }
+
+
+formulario.addEventListener('submit', recebeEventoDoForm) 
+
+}
+
+escopoTotal()
