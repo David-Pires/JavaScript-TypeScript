f220d88a47783b95f4efb88c7dae76325f56ad6c
David Pires <developer.david@outlook.com>
2022-06-02 13:02:09 -0300

    feat: aula20

---
+++ aula20/js/index.js
@@ -0,0 +1,32 @@
+// aula20 - Exercícios 
+
+
+function meuEscopo() {
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');
+
+  const pessoas = []
+
+    function recebeEventoform(evento) {
+      evento.preventDefault()
+
+      const nome = form.querySelector('.nome')
+      const sobrenome = form.querySelector('.sobrenome')
+      const peso = form.querySelector('.peso')
+      const altura = form.querySelector('.altura')
+      
+      pessoas.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value,
+      })
+      console.log(pessoas);
+      
+      
+    }
+
+   form.addEventListener('submit', recebeEventoform)
+} 
+
+meuEscopo();
