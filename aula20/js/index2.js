1ac241f835177c7934c470e599d578b7fc4c541d
David Pires <developer.david@outlook.com>
2022-06-02 14:39:44 -0300

    feat: new practice

--- aula20/js/index2.js
+++ aula20/js/index2.js
@@ -1,34 +1,35 @@
 
-function escopoTotal() {
-  const formulario = document.querySelector('.form')
-  const infoDoform = document.querySelector('.infoDoForm')
+// function escopoTotal() {
+//   const formulario = document.querySelector('.form')
+//   const infoDoform = document.querySelector('.infoDoForm')
 
-  const array = []
+//   const array = []
 
-  function recebeEventoDoForm(evento) {
-    evento.preventDefault()
+//   function recebeEventoDoForm(evento) {
+//     evento.preventDefault()
 
-    const nome = formulario.querySelector('.nome')
-    const sobrenome = formulario.querySelector('.sobrenome')
-    const peso = formulario.querySelector('.peso')
-    const altura = formulario.querySelector('.altura')
+//     const nome = formulario.querySelector('.nome')
+//     const sobrenome = formulario.querySelector('.sobrenome')
+//     const peso = formulario.querySelector('.peso')
+//     const altura = formulario.querySelector('.altura')
     
     
-    array.push({
-      nome: nome.value,
-      sobrenome: sobrenome.value,
-      peso: peso.value,
-      altura: altura.value,
+//     array.push({
+//       nome: nome.value,
+//       sobrenome: sobrenome.value,
+//       peso: peso.value,
+//       altura: altura.value,
 
-    })
-    console.log(array)
-    infoDoform.innerHTML += `${nome.value} ${sobrenome.value} <br />` +
-    `${peso.value} ${altura.value}`
-  }
+//     })
+//     console.log(array)
+//     infoDoform.innerHTML += `${nome.value} ${sobrenome.value} <br />` +
+//     `${peso.value} ${altura.value}`
+//   }
 
 
-formulario.addEventListener('submit', recebeEventoDoForm) 
+// formulario.addEventListener('submit', recebeEventoDoForm) 
 
-}
+// }
 
-escopoTotal()
+// escopoTotal()
+
