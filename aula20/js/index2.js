723c6508513c7e53d529a8d0002f39f4a2f63d47
David Pires <developer.david@outlook.com>
2022-06-02 14:58:10 -0300

    feat: implemented

--- aula20/js/index2.js
+++ aula20/js/index2.js
@@ -33,3 +33,29 @@
 
 // escopoTotal()
 
+function escopoTotal() {
+  const form = document.querySelector('.form')
+  const infoDoForm = document.querySelector('.infoDoForm')
+  const arrayDeInfo = []
+
+    function prevenirComportamentoForm(evento) {
+      evento.preventDefault()    
+      const nome = form.querySelector('.nome')
+      const sobrenome = form.querySelector('.sobrenome')
+      const peso = form.querySelector('.peso')
+      const altura = form.querySelector('.altura')
+
+      arrayDeInfo.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value,
+      })
+      
+      infoDoForm.innerHTML += `${nome.value} ${sobrenome.value}` + 
+      `${peso.value} ${altura.value}`
+    }
+
+  form.addEventListener('submit', prevenirComportamentoForm)
+}
+escopoTotal()
