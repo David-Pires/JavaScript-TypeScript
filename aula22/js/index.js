9d8673a434c8d362ae4583f5c7be84de0a8b4aa9
David Pires <developer.david@outlook.com>
2022-06-02 15:38:21 -0300

    feat: aula22

--- aula22/js/index.js
+++ aula22/js/index.js
@@ -1,8 +1,14 @@
 // aula 22 Operadores Lógicos
 
 /*
+
 && -> AND -> E
 || -> OR - > OU
 ! -> NOT -> NÃO
 
 */
+
+const expressaoEnd = true && true && true // todas expressões necessitam ser verdadeiras para retornar true 
+const expressaoOr = true || false // basta apenas que uma condição seja verdadeira para que retorn true
+console.log(!true); // nega o valor true e o transforma em false 
+
