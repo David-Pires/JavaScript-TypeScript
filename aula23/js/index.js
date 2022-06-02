317dc26485700de288705b7a6cec001b5c49075d
David Pires <developer.david@outlook.com>
2022-06-02 16:17:43 -0300

    feat: aula23

---
+++ aula23/js/index.js
@@ -0,0 +1,18 @@
+// aula 23 Operadores Lógicos
+
+/*
+
+&& -> false && true -> false "o valor mesmo"
+|| -> 
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+console.log('David' && true && 'Pires');
+
+
