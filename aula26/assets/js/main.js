b9625fff9dc6065302bc77643908c3cb6d3780c6
David Pires <developer.david@outlook.com>
2022-06-03 16:18:55 -0300

    feat: tabela imc

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,35 +1,63 @@
+const form = document.querySelector("#formulario");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+  if (!altura) {
+    setResultado("Altura inválido", false);
+    return;
+  }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc)
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`
+  setResultado(msg, true)
+});
 
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
 
-
-function escopoGlobal() {
-  const form = document.querySelector('.form')
-  const info = document.querySelector('.info')
-
-  const infoDoResultado = []
-
-  function comportamentoForm(event) {
-    const peso = document.querySelector('#peso')
-    const altura = document.querySelector('#altura')
-    event.preventDefault()
-
-    console.log(peso.value, altura.value)
-
-    infoDoResultado.push({
-      peso: Number(peso.value),
-      altura: Number(altura.value),     
-      imc: peso / (altura*altura)
-    })
-      
-    console.log(infoDoResultado)
-
-
-      
-    }
-
-    form.addEventListener('submit', comportamentoForm)
-
+  if (imc >= 39.9) return nivel[5];  
+  if (imc >= 34.9) return nivel[4];  
+  if (imc >= 29.9) return nivel[3];  
+  if (imc >= 24.9) return nivel[2];  
+  if (imc >= 18.5) return nivel[1];  
+  if (imc < 18.5) return nível[0];
 }
 
-escopoGlobal()
+function getImc(peso, altura) {
+  const imc = peso / altura ** altura;
+  return imc.toFixed(2);
+}
+function criarP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+  const p = criarP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado')
+  } else {
+    p.classList.add('bad')
+  }
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
