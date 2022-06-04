08a627b7b753218c14c7fda1eb6cc0b60e38a4cb
David Pires <developer.david@outlook.com>
2022-06-04 12:34:24 -0300

    fix: correct files

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,33 +1,63 @@
-//? : ternário
+const form = document.querySelector("#formulario");
 
-// (condicao) ? 'Valor para verdadeiro' : 'Valor para falso'
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-const pontuacaoUsuario = 999;
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+  if (!altura) {
+    setResultado("Altura inválido", false);
+    return;
+  }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc)
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`
+  setResultado(msg, true)
+});
 
-const nivelUsuario = pontuacaoUsuario >= 1000 ? 'usuário VIP' : 'Usuário Normal'
-console.log(nivelUsuario);
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
 
-const euMesmo = 'magro'
+  if (imc >= 39.9) return nivel[5];  
+  if (imc >= 34.9) return nivel[4];  
+  if (imc >= 29.9) return nivel[3];  
+  if (imc >= 24.9) return nivel[2];  
+  if (imc >= 18.5) return nivel[1];  
+  if (imc < 18.5) return nível[0];
+}
 
-const comparacao = euMesmo === 'gordo' ? 'Faça dieta' : 'Continua comendo até ter que fazer dieta'
-console.log(comparacao);
+function getImc(peso, altura) {
+  const imc = peso / altura ** altura;
+  return imc.toFixed(2);
+}
+function criarP() {
+  const p = document.createElement("p");
+  return p;
+}
 
-const novoParametro = 15
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+  const p = criarP();
 
-const comparaParametro = novoParametro <= 10 ? 'Parâmetro está abaixo do permitido' : ' Parâmetro aceitável'
-console.log(comparaParametro);
-
-
-const corUsuario = null
-const corPadrao = corUsuario || 'preta' // ou 
-console.log(corPadrao);
-
-
-
-
-
-// if(pontuacaoUsuario > = 1000) {
-//   console.log('Usuário vip');
-// } else {
-//   console.log('Usuário norma');
-// }
+  if (isValid) {
+    p.classList.add('paragrafo-resultado')
+  } else {
+    p.classList.add('bad')
+  }
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
