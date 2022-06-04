aa4e668f0f43301fbf5948ad8c5f0ca3aec11baf
David Pires <developer.david@outlook.com>
2022-06-04 12:43:02 -0300

    feat: aula28

---
+++ aula28/assets/js/main.js
@@ -0,0 +1,31 @@
+//? : ternário
+
+// (condicao) ? 'Valor para verdadeiro' : 'Valor para falso'
+
+const pontuacaoUsuario = 999;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'usuário VIP' : 'Usuário Normal'
+console.log(nivelUsuario);
+
+const euMesmo = 'magro'
+
+const comparacao = euMesmo === 'gordo' ? 'Faça dieta' : 'Continua comendo até ter que fazer dieta'
+console.log(comparacao);
+
+const novoParametro = 15
+
+const comparaParametro = novoParametro <= 10 ? 'Parâmetro está abaixo do permitido' : ' Parâmetro aceitável'
+console.log(comparaParametro);
+
+
+
+
+
+
+
+
+// if(pontuacaoUsuario > = 1000) {
+//   console.log('Usuário vip');
+// } else {
+//   console.log('Usuário norma');
+// }
