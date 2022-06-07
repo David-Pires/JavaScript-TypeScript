a29ce8bc1711325a338e77382dd47d8a68b8c27e
David Pires WorkPlace <developer.david@outlook.com>
2022-06-07 16:26:28 -0300

    feat: comments

--- aula29/assets/js/main.js
+++ aula29/assets/js/main.js
@@ -57,3 +57,19 @@ console.log(diaSemanaTexto);
 // } else if ( diaSemana === 6) {
 //   diaSemanaTexto = 'Sábado'
 // }
+
+// if (diaSemana === 0) {
+//   diaSemanaTexto = 'Domingo'
+// } else if ( diaSemana === 1) {
+//   diaSemanaTexto = 'Segunda-feira'
+// } else if ( diaSemana === 2) {
+//   diaSemanaTexto = 'terça-feira'
+// } else if ( diaSemana === 3) {
+//   diaSemanaTexto = 'Quarta-feira'
+// } else if ( diaSemana === 4) {
+//   diaSemanaTexto = 'Quinta-feira'
+// } else if ( diaSemana === 5) {
+//   diaSemanaTexto = 'Sexta-feira'
+// } else if ( diaSemana === 6) {
+//   diaSemanaTexto = 'Sábado'
+// }
