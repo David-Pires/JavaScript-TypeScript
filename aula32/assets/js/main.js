5f5db47aeda40e33d80d6bd106f6d5ae3ae485c0
David Pires <developer.david@outlook.com>
2022-06-15 22:26:38 -0300

    feat: aula32  Atribuição via desestruturação

--- aula32/assets/js/main.js
+++ aula32/assets/js/main.js
@@ -1,1 +1,17 @@
 // Aula 32 - Atribuição via desestruturação
+
+
+// let a = 'A' //B
+// let b = 'B' //C
+// let c = 'C' //A
+
+// const letras = [b, c, a];
+// [a, b, c] = letras
+
+// console.log(a, b, c)
+//    índice     0  1  2  3  4  5  6  7  8
+const numeros = [1562, 259, 3, 4, 5, 6, 7, 8, 9];
+
+const [primeiroNumero, segundoNumero, ...resto] = numeros;
+
+console.log(primeiroNumero, segundoNumero, resto);
