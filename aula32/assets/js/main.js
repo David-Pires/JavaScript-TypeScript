2ef772cea898fd0252f17ec8f3d52af7648985a4
David Pires <developer.david@outlook.com>
2022-06-15 23:18:43 -0300

    feat: aula32

--- aula32/assets/js/main.js
+++ aula32/assets/js/main.js
@@ -9,9 +9,12 @@
 // [a, b, c] = letras
 
 // console.log(a, b, c)
-//    índice     0  1  2  3  4  5  6  7  8
-const numeros = [1562, 259, 3, 4, 5, 6, 7, 8, 9];
+//    índice        0  1  2  3  4  5  6  7  8
+// const numeros = [1562, 259, 3, 4, 5, 6, 7, 8, 9];
 
-const [primeiroNumero, segundoNumero, ...resto] = numeros;
+// const [um, ,tres, ,cinco, ,sete] = numeros;
+//                  0           1          2
+//               0   1  2    0  1  2    0  1  2 
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-console.log(primeiroNumero, segundoNumero, resto);
+console.log(numeros[0] [1]);
