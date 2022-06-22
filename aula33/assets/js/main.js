e2ef9671e8f86e4d177c9949a46de1ab9eb12dd8
David Pires <developer.david@outlook.com>
2022-06-21 21:51:10 -0300

    feat: aula33 atribuição via desestruturação

--- aula33/assets/js/main.js
+++ aula33/assets/js/main.js
@@ -1,3 +1,15 @@
 // Aula 33 - Objeto - Atribuição via desestruturação
 
 
+const pessoa = {
+  nome: 'David',
+  sobrenome: 'Pires',
+  idade: 30,
+  endereco: {
+    rua: 'Setor O',
+    numero: 49
+  }
+}
+// atribuição via desestruturação 
+const {nome} = pessoa;
+console.log(nome);
